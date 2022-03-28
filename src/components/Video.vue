59b7e1138f3a5b194cff8cefbdf8ba25c6ec511e
Kevin <kevin.brun.dev@gmail.com>
2022-03-29 01:23:21 +0200

    Témoignages vidéos ok

---
+++ src/components/Video.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  videoSrc: String,
+  name: String,
+  job: String,
+  com: String,
+});
+</script>
+
+<template>
+  <div
+    class="bg-darkblue bg-opacity-80 rounded-3xl flex flex-col items-center text-white p-2 ring-1 ring-yellowIg"
+  >
+    <div class="text-center">
+      <h2 class="text-2xl font-bold">{{ name }}</h2>
+      <p class="text-xl font-semibold">{{ job }}</p>
+    </div>
+    <div>
+      <video
+        :src="videoSrc"
+        type="video/mp4"
+        width="300"
+        controls
+        controlslist=" nodownload nofullscreen"
+        class="rounded-2xl"
+      ></video>
+    </div>
+    <cite class="text-center">{{ com }}</cite>
+  </div>
+</template>
---
+++ src/views/Temoignage.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import Video from "../components/Video.vue";
+</script>
+<template>
+  <main>
+    <Video
+      videoSrc="../../src/assets/videos/david_temoignage.mp4"
+      name="David Lefrançois"
+      job="Psychologue Formateur"
+      com="Rarement vu quelqu'un qui maîtrisait autant toute la matière"
+      class="m-2 mt-8"
+    />
+    <Video
+      videoSrc="../../src/assets/videos/temoignage_bakari.mp4"
+      name="Bakari Niakate"
+      job="Entrepreneur Coach"
+      com="Un homme au grand coeur"
+      class="m-2"
+    ></Video>
+    <Video
+      videoSrc="../../src/assets/videos/temoignage_monique.mp4"
+      name="Monique Sallaz"
+      job="Docteur Neuroscience"
+      com="Une vrai intelligence cognitive"
+      class="m-2"
+    ></Video>
+
+    <router-link to="/">
+      <font-awesome-icon
+        icon="chevron-up"
+        class="text-white fixed top-0 left-44"
+        size="2x"
+      />
+    </router-link>
+  </main>
+</template>
